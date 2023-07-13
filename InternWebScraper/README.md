66d4698338f78b4785b6765780867e4c8629c635
Filip <101744276+Hunter2k02@users.noreply.github.com>
2023-07-13 15:08:38 +0200

    Add files via upload
    Version 0.2 
    Added noFluffJobs scrapp, and exception in pracuj.pl

---
+++ InternWebScraper/README.md
@@ -0,0 +1,6 @@
+# InternWebScraper
+# Version 0.2
+
+# Creators:
+# Szymon Fit - https://github.com/SzymonFit
+# Filip Sanecki - https://github.com/Hunter2k02
---
+++ InternWebScraper/app.py
@@ -0,0 +1,12 @@
+from Scrapper import Scrapper
+
+
+Scrap = Scrapper()
+
+titles1 = Scrap.pracuj()
+#titles2 = Scrap.noFluffJobs()
+
+
+# print(titles1['pracuj']['Internship Global Security'])
+
+
