44a20795b49ef8678434be6d9ebf75d964c28d9d
Filip <101744276+Hunter2k02@users.noreply.github.com>
2023-07-29 13:30:30 +0200

    Add files via upload

---
+++ InternWebScrapper/Scrapper.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+class Scrapper:
+    
+    def __init__(self) -> None:
+        self.list_of_urls = ["https://www.pracuj.pl/praca/intern%20it;kw/krakow;wp?rd=0&et=1","https://nofluffjobs.com/pl/krakow?page=1&criteria=seniority%3Dtrainee"]
+        self.main_dict = {'pracuj':None, 'nofluffjobs':None}
+        self.noFluffJobs()
+        self.pracuj()
+        
+    def noFluffJobs(self):
+
+        url = self.list_of_urls[1]
+        titles = []
+
+        while True:
+
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            h3_elements = soup.select('nfj-postings-list h3')
+            text_values = [h3.get_text(strip=True) for h3 in h3_elements]
+            
+            a_elements = soup.select('.posting-list-item')
+            href_values = [element['href'] for element in a_elements]
+            base_url = "https://nofluffjobs.com" 
+                    
+            for x in range(len(text_values)):
+                try:
+                    if x < len(href_values):
+                        titles.append({'title': text_values[x], "link":(base_url + href_values[x])})
+                except AttributeError:
+                    None
+
+            pagination = soup.select('a[aria-label="Next"]')
+            if pagination:
+                url = base_url + pagination[0]['href']
+            else:
+                break
+
+        self.main_dict['nofluffjobs'] = titles                
+        
+    def pracuj(self):
+        
+        titles = []
+        parent_url = self.list_of_urls[0]
+
+        while True:
+            
+            response = requests.get(self.list_of_urls[0])
+            soup = BeautifulSoup(response.text, "html.parser")
+            list_of_headings = soup.find_all("h2", class_="listing_buap3b6")
+            
+            difference = len(parent_url)-len(self.list_of_urls[0])
+            
+            for x in range(len(list_of_headings)):
+                try:
+                    heading = list_of_headings[x].find("a")
+                    titles.append({'title': heading.text, "link":heading["href"]})
+                except AttributeError:
+                    None
+
+            if soup.find(class_="listing_n5kktfs size-small variant-secondary listing_b1fqykql"):   
+                arr = list(self.list_of_urls[0])
+                digit = int(''.join(arr[(difference-1):]))
+                digit+=1
+                arr[(difference-1):] = str(digit)
+                self.list_of_urls[0] = ''.join(arr)
+            else:
+                break
+            
+        self.main_dict['pracuj'] = titles
+
+
+    def json(self):
+        with open("plik.json", "w", encoding = "UTF-8") as jf:
+            json.dump(self.main_dict, jf, indent = 4, ensure_ascii=False)
+            
+if __name__ == "__main__":           
+    scrap = Scrapper()
+    scrap.json()
---
+++ README.md
@@ -0,0 +1,19 @@
+# InternWebScraper
+
+##
+Project is done with VS Code live share
+
+## Creators
+Szymon Fit - https://github.com/SzymonFit
+Filip Sanecki - https://github.com/Hunter2k02
+
+## Technologies Used
+Python, unittest, beautifulsoup, Flask, Jinja2
+
+## Stages of project work
+
+1. Downloading links and offer titles from the pracuj.pl and nofluffjobs.pl websites
+2. Create a function which creates file that stores all of links and titles
+3. Changes in both pracuj and nofluff funcion which allows to move to other pages
+4. Code optimization
+5. Create a tests for scrapper class
